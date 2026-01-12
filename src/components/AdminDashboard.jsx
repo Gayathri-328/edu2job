f22b7d9f124373350e0235fafd7eba2b7b6305d4
226W1A05A4 <satyamgayathri99@gmail.com>
2026-01-12 16:04:38 +0530

    upload

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./AdminDashboard.css";
-// ================= DUMMY FEEDBACK DATA =================
+// =================  FEEDBACK DATA =================
 const dummyFeedbacks = [
   {
     name: "Anjali Sharma",
